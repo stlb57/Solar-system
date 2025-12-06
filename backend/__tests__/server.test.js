aa54df6787c1a3215955855ddd18bad12255ac72
lb57 <bablubhaiya@gmail.com>
2025-12-06 20:25:57 +0530

    test for phase 2

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -5,7 +5,9 @@ describe('Telemetry Service', () => {
   });
 
   // Uncomment this to simulate a failure for Module 7 (Conditionals)
+  /*
   test('Simulate Failure', () => {
     expect(true).toBe(false);
   });
+  */
 });
